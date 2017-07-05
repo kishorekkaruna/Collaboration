03e15f06cf74e56209f51006f85c9f78ab7667ae
Kishore Kumar K <kishorekaruna253@gmail.com>
2017-07-05 16:59:37 +0530

    Add files via upload

--- RestController/src/main/java/com/niit/collaborationbackend/controller/JobController.java
+++ RestController/src/main/java/com/niit/collaborationbackend/controller/JobController.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.niit.collaborationbackend.dao.JobDAO;
 import com.niit.collaborationbackend.model.Job;
+import com.niit.collaborationbackend.model.User;
 
 @RestController
 public class JobController {
@@ -39,6 +42,23 @@ public class JobController {
 	public ResponseEntity<Job> save(@RequestBody Job job) {
 		jobDAO.save(job);
 		return new ResponseEntity<Job>(job, HttpStatus.OK);
+	}
+	
+	@PutMapping("/job")
+	public ResponseEntity<Job> update(@RequestBody Job job) {
+		jobDAO.saveOrUpdate(job);
+		return new ResponseEntity<Job>(job, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/job/{jobId}")
+	public ResponseEntity deleteJob(@PathVariable("jobId") int id) {
+		Job job = jobDAO.getByJobId(id);
+		if (job == null) {
+			return new ResponseEntity("No Job found for ID " + id, HttpStatus.NOT_FOUND);
+		}
+		jobDAO.delete(id);
+		return new ResponseEntity("deleted for ID " + id, HttpStatus.OK);
+
 	}
 
 }
